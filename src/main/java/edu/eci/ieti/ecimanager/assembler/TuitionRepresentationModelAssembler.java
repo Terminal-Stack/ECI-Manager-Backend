d03d951f6b0a8eb2e055a811da170839d942a5b5
Daniel Vela <de.vela.lopez@gmail.com>
2020-04-30 13:17:31 -0500

    Added tuitions to the API

--- src/main/java/edu/eci/ieti/ecimanager/assembler/TuitionRepresentationModelAssembler.java
+++ src/main/java/edu/eci/ieti/ecimanager/assembler/TuitionRepresentationModelAssembler.java
@@ -2,6 +2,7 @@ package edu.eci.ieti.ecimanager.assembler;
 
 import edu.eci.ieti.ecimanager.controller.TuitionController;
 import edu.eci.ieti.ecimanager.model.Student;
+import edu.eci.ieti.ecimanager.model.Tuition;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
@@ -12,12 +13,12 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
  * @author Daniel Vela
  */
 @Component
-public class StudentRepresentationModelAssembler implements RepresentationModelAssembler<Student, EntityModel<Student>> {
+public class TuitionRepresentationModelAssembler implements RepresentationModelAssembler<Tuition, EntityModel<Tuition>> {
 
     @Override
-    public EntityModel<Student> toModel(Student student) {
-        return new EntityModel<Student>(student,
-                linkTo(TuitionController.class).slash(student.getCollegeId()).withSelfRel(),
+    public EntityModel<Tuition> toModel(Tuition student) {
+        return new EntityModel<Tuition>(student,
+                linkTo(TuitionController.class).slash(student.getStudentCollegeId()).withSelfRel(),
                 linkTo(TuitionController.class).withRel("students"));
     }
 }
