724a1bf297922d063419a4415eee5cf9e0b3c4ec
Daniel Vela <de.vela.lopez@gmail.com>
2020-05-21 01:06:57 -0500

    idfk anymore

--- src/main/java/edu/eci/ieti/ecimanager/config/JWTWebSecurityConfig.java
+++ src/main/java/edu/eci/ieti/ecimanager/config/JWTWebSecurityConfig.java
@@ -63,8 +63,7 @@ public class JWTWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf().disable()
-                .cors().configurationSource(corsConfigurationSource()).and()
+                .cors().and()
                 .exceptionHandling().authenticationEntryPoint(jwtUnAuthorizedResponseAuthenticationEntryPoint).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
